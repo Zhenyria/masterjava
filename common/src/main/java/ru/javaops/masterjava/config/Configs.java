3cf524089ace3509666821e3e9e7c81f1090c80d
Eugene Aribzhanov <catshaurma@gmail.com>
2022-10-19 23:22:40 +0600

    Make simple cash for configs

--- common/src/main/java/ru/javaops/masterjava/config/Configs.java
+++ common/src/main/java/ru/javaops/masterjava/config/Configs.java
@@ -2,15 +2,27 @@ package ru.javaops.masterjava.config;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import lombok.experimental.UtilityClass;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * gkislin
  * 01.11.2016
  */
+@UtilityClass
 public class Configs {
+    private static final Map<String, Config> inMemoryConfigs = new ConcurrentHashMap<>();
 
     public static Config getConfig(String resource) {
-        return ConfigFactory.parseResources(resource).resolve();
+        Config config = inMemoryConfigs.get(resource);
+        if (Objects.isNull(config)) {
+            config = ConfigFactory.parseResources(resource).resolve();
+            inMemoryConfigs.put(resource, config);
+        }
+        return config;
     }
 
     public static Config getConfig(String resource, String domain) {
