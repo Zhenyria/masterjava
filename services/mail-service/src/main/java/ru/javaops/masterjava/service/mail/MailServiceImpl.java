d5ba6b0fe69491086ba0e728766f39c0593b0082
Eugene Aribzhanov <catshaurma@gmail.com>
2023-08-19 20:16:09 +0600

    Add handling for wsdl as external resource

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceImpl.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceImpl.java
@@ -3,9 +3,9 @@ package ru.javaops.masterjava.service.mail;
 import javax.jws.WebService;
 import java.util.Set;
 
-@WebService(endpointInterface = "ru.javaops.masterjava.service.mail.MailService", targetNamespace = "http://mail.javaops.ru/"
-//          , wsdlLocation = "WEB-INF/wsdl/mailService.wsdl"
-)
+@WebService(endpointInterface = "ru.javaops.masterjava.service.mail.MailService",
+            targetNamespace = "http://mail.javaops.ru/",
+            wsdlLocation = "mailService.wsdl")
 public class MailServiceImpl implements MailService {
     public String sendToGroup(Set<Addressee> to, Set<Addressee> cc, String subject, String body) {
         return MailSender.sendToGroup(to, cc, subject, body);
